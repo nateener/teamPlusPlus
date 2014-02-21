388d68447e8fa9b050580e103e566191fb4a955b
cmsmirga <cmsmirga@gmail.com>
2014-02-20 23:52:36 -0500

    View Iteration 1
    Started the View class for iteration 1. Added menus, toolbars, and the
    draw panel, but no functionality for the buttons. I had to add a
    placeholder of the Controller in order to run the program. It should
    compile and run clean.

---
+++ UML Editor/src/View.java
@@ -0,0 +1,156 @@
+/*
+ * File: View.java
+ * Project: UML Editor
+ * Iteration: 1
+ * Description: The View class is an extension of the JFrame class in the Swing API, as per the Java Model-View-Control
+ * conventions for design. It is designed to be as inclusive as possible; managing the view port, window, frames, panels
+ * tool bars, and menu bar. 
+ */
+
+/*
+ * TODO Add action calls for menu items and tool bar buttons.
+ * TODO Add mouse listener to draw panel.
+ * TODO Add icons for tool bar buttons.
+ */
+
+/* Imports */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+@SuppressWarnings("serial")
+public class View extends JFrame {
+	
+	/* Default constructor for the View class.
+	 * Parameters: None
+	 * Return: Void
+	 * Sets up the 'Look And Feel' in the UI Manager.
+	 * Calls the build for the tool bars, menu bars, and draw panels.
+	 * Initializes the display settings.
+	 */
+	public View() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+		buildMenuBar();
+		buildToolBar();
+		buildDrawPanel();
+		this.setTitle("UML Editor v1.0");
+		this.setSize(800,600);
+		this.setVisible(true);
+	}
+	
+	/* Builder function for the menu bar.
+	 * Parameters: None
+	 * Return: Void
+	 * Generates the menu bar, menus, and menu items.
+	 */
+	private void buildMenuBar() {
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenu subMenu;
+		JMenuItem item;
+		menuBar = new JMenuBar();
+		menu = new JMenu("File");
+		menuBar.add(menu);
+		item = new JMenuItem("New");
+		item.setMnemonic(KeyEvent.VK_N);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		item = new JMenuItem("Open...");
+		item.setMnemonic(KeyEvent.VK_O);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		menu.addSeparator();
+		item = new JMenuItem("Save");
+		item.setMnemonic(KeyEvent.VK_S);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		menu.add(item);
+		item = new JMenuItem("Save As...");
+		item.setMnemonic(KeyEvent.VK_A);
+		item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
+		menu.add(item);
+		menu.addSeparator();
+		item = new JMenuItem("Exit");
+		item.setIcon(new ImageIcon("exit.png"));
+		menu.add(item);
+		menu = new JMenu("Tools");
+		menuBar.add(menu);
+		subMenu = new JMenu("Add Node");
+		subMenu.setMnemonic(KeyEvent.VK_N);
+		menu.add(subMenu);
+		item = new JMenuItem("Class");
+		item.setMnemonic(KeyEvent.VK_C);
+		subMenu.add(item);
+		subMenu = new JMenu("Add Association");
+		subMenu.setMnemonic(KeyEvent.VK_A);
+		menu.add(subMenu);
+		item = new JMenuItem("Aggregation");
+		item.setMnemonic(KeyEvent.VK_G);
+		subMenu.add(item);
+		item = new JMenuItem("Composition");
+		item.setMnemonic(KeyEvent.VK_C);
+		subMenu.add(item);
+		menu.addSeparator();
+		item = new JMenuItem("Delete");
+		item.setMnemonic(KeyEvent.VK_D);
+		menu.add(item);
+		this.setJMenuBar(menuBar);
+	}
+	
+	/* Builder function for the tool bar.
+	 * Parameters: None
+	 * Return: Void
+	 * Generates the tool bar and buttons.
+	 */
+	private void buildToolBar() {
+		JToolBar toolBar;
+		JButton button;
+		toolBar = new JToolBar(JToolBar.VERTICAL);
+		toolBar.setFloatable(false);
+		add(toolBar, BorderLayout.LINE_START);
+		button = new JButton("Class");
+		toolBar.add(button);
+		toolBar.addSeparator();
+		button = new JButton("Aggregation");
+		toolBar.add(button);
+		button = new JButton("Composition");
+		toolBar.add(button);
+		toolBar.addSeparator();
+		button = new JButton("Delete");
+		toolBar.add(button);
+	}
+	
+	/* Builder function for the tool bar.
+	 * Parameters: None
+	 * Return: Void
+	 * Generates the draw panel.
+	 */
+	private void buildDrawPanel() {
+		JPanel drawPanel = new JPanel();
+		drawPanel.setBackground(new Color(255, 255, 255));
+		add(drawPanel, BorderLayout.CENTER);
+	}
+
+}
