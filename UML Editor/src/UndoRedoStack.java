d8b98520e2163dd79d684a87b4009283557c64ee
jcard <emong_gnome@yahoo.com>
2014-04-09 02:42:27 -0400

    Added some explanitory/reminder comments

--- UML Editor/src/UndoRedoStack.java
+++ UML Editor/src/UndoRedoStack.java
@@ -29,6 +29,9 @@ public class UndoRedoStack {
 	 * 		The State object that was placed on the stack.
 	 */
 	public State undoPush(ArrayList<Node> nodes, ArrayList<Relationship> rels) {
+		/*TODO: Create a copy of nodes/rels to push instead of just pushing them.
+		Figure out if there's a less painful way to copy relationships. 
+		Also: Maybe use a static long in Node and Relationship to create unique ids for them to ease equality checking*/
 		return undoStack.push(new State(nodes, rels));
 	}
 	
